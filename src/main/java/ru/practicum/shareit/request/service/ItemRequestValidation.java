911ab4e5e913cb1478633d0cf445cb7952bad14f
Егор <Egorog163@yandex.ru>
2023-03-18 20:23:18 +0300

    16 sprint 2 commit + refactor (query, маппинг остались)

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestValidation.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestValidation.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.request.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.request.dto.RequestBodyItemRequestDto;
--- src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.practicum.shareit.TestHelper;
 import ru.practicum.shareit.item.ItemRepository;
 
